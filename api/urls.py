9dd6d03805a4329c240eee6a756d48367d36509d
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-08-20 19:33:46 -0500

    rotes updated

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^uniquecostassign/', views.generarAsignacion, name = 'generarAsignacionSimple'),
-    url(r'^projects|userstories|sprints|developers$', views.gestionGeneralProyectos, name = 'generarAsignacionSimple'),
+    url(r'^uniquecostassign/', views.generarAsignacion, name='generarAsignacion'),
+    url(r'^attributeassign/', views.generate_assignment_with_attributes, name='generate_assignment_with_attributes'),
+    url(r'^pairassign/', views.generate_pair_assignment, name='pairassign'),
+    url(r'^projects|userstories|sprints|developers$', views.general_project_management, name='generarAsignacionSimple'),
 ]
--- api/views.py
+++ api/views.py
@@ -10,21 +10,40 @@ import json
 # Create your views here.
 @api_view(['POST'])
 @permission_classes((AllowAny, ))
-def generarAsignacion(request, format=None):        
-    remoteurl = 'http://user-stories-assignment:3001/uniquecostassign/'
+def generate_assignment_with_attributes(request):
+    remote_url = 'http://user-stories-assignment:3001/attributeassign/'
     print(request.data)
-    response = requests.post(remoteurl, json=request.data)
+    response = requests.post(remote_url, json=request.data)
     return JsonResponse(json.loads(response.text), safe=False)
+
+
+@api_view(['POST'])
+@permission_classes((AllowAny, ))
+def generarAsignacion(request):
+    remote_url = 'http://user-stories-assignment:3001/uniquecostassign/'
+    print(request.data)
+    response = requests.post(remote_url, json=request.data)
+    return JsonResponse(json.loads(response.text), safe=False)
+
+
+@api_view(['POST'])
+@permission_classes((AllowAny, ))
+def generate_pair_assignment(request):
+    remote_url = 'http://user-stories-assignment:3001/pairassign/'
+    print(request.data)
+    response = requests.post(remote_url, json=request.data)
+    return JsonResponse(json.loads(response.text), safe=False)
+
 
 @api_view(['GET', 'PATCH'])
 @permission_classes((AllowAny, ))
-def gestionGeneralProyectos(req, format=None):
-	headers = {"Authorization":"Bearer eyJ1c2VyX2F1dGhlbnRpY2F0aW9uX2lkIjozMDM0NTZ9:1fKuoT:9O26jWVYE63vCuzbrjCsAo5nM1Y"}        
-	remoteurl = 'http://project-managers-interface:3000' + req.path.replace('/api/v1', '')
-	response = {}
-	if req.method == 'PATCH':
-		print(req.data, 'data desde python')
-		response = requests.patch(remoteurl, json=req.data, params = req.GET, headers = headers)
-	if req.method == 'GET':
-		response = requests.get(remoteurl, params = req.GET, headers = headers)
-	return JsonResponse(json.loads(response.text),  safe=False, status=response.status_code)
+def general_project_management(req):
+    headers = {"Authorization":"Bearer eyJ1c2VyX2F1dGhlbnRpY2F0aW9uX2lkIjozMDM0NTZ9:1fKuoT:9O26jWVYE63vCuzbrjCsAo5nM1Y"}
+    remote_url = 'http://project-managers-interface:3000' + req.path.replace('/api/v1', '')
+    response = {}
+    if req.method == 'PATCH':
+        print(req.data, 'data desde python')
+        response = requests.patch(remote_url, json=req.data, params=req.GET, headers=headers)
+    if req.method == 'GET':
+        response = requests.get(remote_url , params=req.GET, headers=headers)
+    return JsonResponse(json.loads(response.text),  safe=False, status=response.status_code)
